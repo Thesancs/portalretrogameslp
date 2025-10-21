6caa47e119014bf3d22f142cafa2a245676b5022
Sancs Agencia <agenciasancs@gmail.com>
2025-10-21 15:53:00 +0000

    Quero que a header seja fixa e em glassmorphism (_for element header_)

--- src/components/app/header.tsx
+++ src/components/app/header.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header className="py-4 bg-card border-b sticky top-0 z-50">
+    <header className="py-4 bg-card/80 border-b border-white/10 fixed top-0 z-50 w-full backdrop-blur-sm">
       <div className="container mx-auto flex flex-col items-center justify-center">
         <Link href="/">
           <Image
             src="/image/logo.png"
             alt="Portal Retro Games Logo"
-            width={180}
+            width={78}
             height={32}
             priority
           />
